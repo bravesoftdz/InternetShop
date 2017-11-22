e6f869fae328a6a60ed88c16a9e2ff1f70ddeb54
ShcIZZiKK <ShcIZZO@mail.ru>
2017-11-22 12:39:20 +0700

    Creator

--- AplicationLogic.pas
+++ AplicationLogic.pas
@@ -3,54 +3,159 @@ unit AplicationLogic;
 interface
 
 type
-  TElement = class;
   TConfiguration = class;
+  TDDR = class;
+  TMotherBoard = class;
+  TProcessor = class;
+
 
   TComputer = class
-  private
-    /// <link>aggregation</link>
-    Configuration: TConfiguration;
   public
     function Select: TComputer; virtual; abstract;
+  published
+    constructor create(Configuration: TConfiguration); virtual; abstract;
   end;
 
   TServer = class(TComputer)
   public
+    /// <link>aggregation</link>
+    fConfiguration: TConfiguration;
     function Select: TComputer; override;
+  published
+    constructor create(Configuration: TConfiguration); override;
   end;
 
   TPortable = class(TComputer)
   public
+    /// <link>aggregation</link>
+    fConfiguration: TConfiguration;
     function Select: TComputer; override;
+  published
+    constructor create(Configuration: TConfiguration); override;
   end;
 
   TDecktop = class(TComputer)
   public
+    /// <link>aggregation</link>
+    fConfiguration: TConfiguration;
     function Select: TComputer; override;
+  published
+    constructor create(Configuration: TConfiguration); override;
   end;
 
   TConfiguration = class
   public
     /// <link>aggregation</link>
-    Element: TElement;
+    fDDR: TDDR;
+    /// <link>aggregation</link>
+    fMotherBoard: TMotherBoard;
+    /// <link>aggregation</link>
+    fProcessor: TProcessor;
+  published
+    constructor create(DDR: TDDR; MotherBoard: TMotherBoard; Processor: TProcessor);
   end;
 
-  TElement = class
+  TDDR = class
+  private
+    name: string;
+    price: integer;
+  published
+    constructor create;
   end;
 
-  TDDR = class(TElement)
+  TMotherBoard = class
+  private
+    name: string;
+    price: integer;
+  published
+    constructor create;
   end;
 
-  TMotherBoard = class(TElement)
-  end;
-
-  TProcessor = class(TElement)
+  TProcessor = class
+  private
+    name: string;
+    price: integer;
+  published
+    constructor create;
   end;
 
 implementation
 
-function TComputer.Select: TComputer;
+
+{ TServer }
+
+constructor TServer.create(Configuration: TConfiguration);
 begin
+  fConfiguration:= Configuration;
+end;
+
+function TServer.Select: TComputer;
+begin
+//
+end;
+
+{ TPortable }
+
+constructor TPortable.create(Configuration: TConfiguration);
+begin
+  fConfiguration:= Configuration;
+end;
+
+function TPortable.Select: TComputer;
+begin
+//
+end;
+
+{ TDecktop }
+
+constructor TDecktop.create(Configuration: TConfiguration);
+begin
+  fConfiguration:= Configuration;
+end;
+
+function TDecktop.Select: TComputer;
+begin
+//
+end;
+
+{ TConfiguration }
+
+constructor TConfiguration.create(DDR: TDDR; MotherBoard: TMotherBoard;
+  Processor: TProcessor);
+begin
+  fDDR:= DDR;
+  fMotherBoard:= MotherBoard;
+  fProcessor:= Processor
+end;
+
+{ TDDR }
+
+constructor TDDR.create;
+begin
+    {
+    name: string;
+    price: integer;
+    }
+end;
+
+{ TMotherBoard }
+
+constructor TMotherBoard.create;
+begin
+    {
+    name: string;
+    price: integer;
+    }
+end;
+
+{ TProcessor }
+
+constructor TProcessor.create;
+begin
+    {
+    name: string;
+    price: integer;
+    }
 end;
 
 end.
--- Unit3.pas
+++ Unit3.pas
@@ -4,14 +4,17 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  AplicationLogic;
 
 type
   TForm3 = class(TForm)
   private
-    { Private declarations }
+    /// <link>aggregation</link>
+    Computer: TComputer;
   public
-    { Public declarations }
+  published
+    constructor create(AOwner: TComponent); override;
   end;
 
 var
@@ -20,5 +23,14 @@ var
 implementation
 
 {$R *.dfm}
+
+{ TForm3 }
+
+constructor TForm3.create(AOwner: TComponent);
+begin
+  inherited;
+  Computer := TPortable.create(TConfiguration.create(TDDR.Create,
+  TMotherBoard.Create, TProcessor.Create));
+end;
 
 end.
